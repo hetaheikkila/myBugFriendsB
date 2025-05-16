9dd9d64aaacc5e32acaae0e00277d5c008387d84
Heta <hhmj28@gmail.com>
2025-05-16 15:38:15 +0300

    First commit

---
+++ mybugfriends/src/main/java/com/com/mybugfriends/MybugfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.com.mybugfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybugfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybugfriendsApplication.class, args);
+	}
+
+}
---
+++ mybugfriends/src/test/java/com/com/mybugfriends/MybugfriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.mybugfriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybugfriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
